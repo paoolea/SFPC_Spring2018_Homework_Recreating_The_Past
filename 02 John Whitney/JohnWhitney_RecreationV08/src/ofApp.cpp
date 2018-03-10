fbf14762762e2591215d68b7204bd36b1835ed2f
pao <paola.g.olea@gmail.com>
2018-03-10 02:02:38 -0500

    Week 2

--- 02 John Whitney/JohnWhitney_RecreationV08/src/ofApp.cpp
+++ 02 John Whitney/JohnWhitney_RecreationV08/src/ofApp.cpp
@@ -29,9 +29,9 @@ void ofApp::draw(){
         myWidthMax = 300 - (i*30);
         float radius = ofMap(sin(time*10), -1, 1, 0, myWidthMax);
         float angle = ofGetElapsedTimef()*(i*2);
-        float color = 255 - (i * 55);
+        float color = 255 - (i * 45);
         
-        ofSetColor(220, 220, 255, color);
+        ofSetColor(223,255, 255, color);
         
         ofPoint pt;
         pt.x = xorig + radius * cos(angle);
@@ -39,78 +39,17 @@ void ofApp::draw(){
         
         line[i].addVertex(pt);
         
-        if (line[i].size() > 150){
+        if (line[i].size() > i*30){
             line[i].getVertices().erase(line[i].getVertices().begin());
         }
         
         line[i].draw();
         
         ofSetColor(255);
-       // ofDrawCircle(pt, 5);
-        
-       /* for(int f = 0; f < 10; f++){
-            float nX = pt.x - (5 * f);
-            float nY = pt.y + (15 * f);
-            
-            ofDrawCircle(nX, nY, 5);
-        }
-        */
-        
-        
-        
-        
+        ofDrawCircle(pt, 5);
+     
     }
-    
-    for(int f = 1; f < 3; f++){
-        myWidthMax = 300 - (f*30);
-        float radiusA = ofMap(sin((time*10) + f), -1, 1, 0, myWidthMax);
-        float angleA = ofGetElapsedTimef()*(f*2);
-        
-        
-        ofPoint ptA;
-        ptA.x = xorig + radiusA * cos(angleA);
-        ptA.y = yorig + radiusA * sin(angleA);
-        
-        
-        ofSetColor(255);
-        ofDrawCircle(ptA, 5);
-        
-        
-    }
-  
-   /*
-    for(int i = 1; i < 2; i++){
-        float xorig = 400;
-        float yorig = 400;
-        float myWidthMax = 300;
-        
-        float sinOfTimeX = sin((time * (10+i))*0.1);
-        float x = ofGetHeight() / 2 + ofMap(sinOfTimeX, -1, 1, -200, 200);
-        
-        float sinOfTimeY = sin((time * (10+i))*0.3);
-        //float y = ofGetHeight() / 2 + ofMap(sinOfTimeY, -1, 1, -200, 200);
-        float y = ofGetHeight()/2;
-        
-        float radius = ofMap(sin(time), -1, 1, 0, myWidthMax);
-        float angle = ofGetElapsedTimef()*(i*2);
-        float color = 255 - (i * 45);
-        //float x = ofGetWidth()/2;
-        //float y = ofGetHeight() / 2 + ofMap(sin(radius), -1, 1, 0, 200);
-        
-        ofSetColor(255);
-        ofDrawCircle(x, y, 5);
-        
-        float xA = myWidthMax * cos(theta);
-        float yA = myWidthMax * sin(theta);
-        
-
-        // Adjust for center of window
-        ofDrawCircle(xA + ofGetWidth()/2, yA + ofGetHeight()/2, 16, 16);
-        
-        // Increment the angle
-        theta += 0.01;
-    }
-    */
+   
 }
 
 //--------------------------------------------------------------
